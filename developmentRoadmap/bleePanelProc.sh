9880f77958f9396a75bed38bfb0ee1fc15e2c39e
Mohsen Banan <form@mohsen.banan.1.byname.net>
2021-06-24 21:39:17 -0700

    bx-gitRepos-auto

--- developmentRoadmap/bleePanelProc.sh
+++ developmentRoadmap/bleePanelProc.sh
@@ -16,7 +16,7 @@ SEED="
 *  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedBleePanelProc.sh]] | 
 "
 FILE="
-*  /This File/ :: /bisos/panels/blee-dev/bleePanelProc.sh 
+*  /This File/ :: /bisos/panels/blee-dev/developmentRoadmap/bleePanelProc.sh 
 "
 if [ "${loadFiles}" == "" ] ; then
     /bisos/core/bsip/bin/seedBleePanelProc.sh -l $0 "$@" 
